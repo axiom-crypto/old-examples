8648766d4645f3de23e509385b292b6a64f08c51
Yu Jiang Tham <yoojbruin@gmail.com>
2023-08-14 07:44:43 -0700

    Fix typo

--- historic-token-balance/src/components/balancesForm/FloatingInfoOverlay.tsx
+++ historic-token-balance/src/components/balancesForm/FloatingInfoOverlay.tsx
@@ -14,7 +14,7 @@ export default function FloatingInfoOverlay() {
         i
       </div>
       <div className={`inline-flex ${show ? "" : "hidden"} absolute z-10 top-[-2em] right-6 px-4 py-2 w-[33em] text-sm flex-wrap border-[1px] border-highlight bg-[rgba(0,0,0,0.9)]`}>
-        {"Choose the deployed contract's slot number that corresponds to "} <span className="font-mono pr-2">{" mapping (address => uint256) balance "}</span> 
+        {"Choose the deployed contract's slot number that corresponds to "} <span className="font-mono pr-2">{" mapping (address => uint256) balances "}</span> 
         {" (ERC20) or "} <span className="font-mono pr-2">{" mapping (address => uint256) balanceOf "}</span> {" (ERC721)."}
       </div>
     </>
