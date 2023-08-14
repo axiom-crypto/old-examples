8de9aa45e3ba670d0403d2b6769bdf319a6e0b17
Yu Jiang Tham <yoojbruin@gmail.com>
2023-08-13 23:49:14 -0700

    Update info overlay

--- historic-token-balance/src/app/page.tsx
+++ historic-token-balance/src/app/page.tsx
@@ -1,4 +1,5 @@
 import BalancesForm from '@/components/balancesForm/BalancesForm';
+import Link from 'next/link';
 
 interface PageProps {
   params: Params;
@@ -24,7 +25,11 @@ export default async function Home({ searchParams }: PageProps) {
         Historic Token Balance Example
       </div>
       <div>
-        { "Get the historic token balance(s) for any Ethereum address. For each ERC-20 token contract address, enter a historic block number as well. Specify up to eight block number and contract address pairs below (AxiomV1Query supports up to 64 rows, but we use 8 for simplicity in this example)." }
+        { "Get the historic token balance(s) for any Ethereum address. For each ERC-20 or ERC-721 contract address, enter a historic block number and "} 
+        <Link href="https://docs.axiom.xyz/developers/sending-a-query/finding-storage-slots" target="_blank">
+          slot of the balance mapping
+        </Link>
+        { " as well. Specify up to eight block number and contract address pairs below (AxiomV1Query supports up to 64 rows, but we use 8 for simplicity in this example)." }
       </div>
       <BalancesForm defaultAddress={address} />
     </>
--- historic-token-balance/src/components/balancesForm/FloatingInfoOverlay.tsx
+++ historic-token-balance/src/components/balancesForm/FloatingInfoOverlay.tsx
@@ -7,13 +7,13 @@ export default function FloatingInfoOverlay() {
   return (
     <>
       <div 
-        className="relative border-[1px] border-text bg-buttonbg rounded-full font-mono text-xs px-1"
+        className={`relative border-[1px] ${show ? "border-highlight text-highlight" : "border-text text-text"} bg-buttonbg rounded-full font-mono text-xs px-1`}
         onMouseOver={() => setShow(true)}
         onMouseOut={() => setShow(false)}
       >
         i
       </div>
-      <div className={`inline-flex ${show ? "" : "hidden"} absolute z-10 top-[-2em] right-6 px-4 py-2 w-[33em] text-sm flex-wrap border-[1px] border-highlight bg-[rgba(0,0,0,0.8)]`}>
+      <div className={`inline-flex ${show ? "" : "hidden"} absolute z-10 top-[-2em] right-6 px-4 py-2 w-[33em] text-sm flex-wrap border-[1px] border-highlight bg-[rgba(0,0,0,0.9)]`}>
         {"Choose the deployed contract's slot number that corresponds to "} <span className="font-mono pr-2">{" mapping (address => uint256) balance "}</span> 
         {" (ERC20) or "} <span className="font-mono pr-2">{" mapping (address => uint256) balanceOf "}</span> {" (ERC721)."}
       </div>
